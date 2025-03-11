a4018d6290c59c5244f5de47583f69574905bdb8
William T Daniel <william.daniel@nottingham.ac.uk>
2025-03-11 08:26:14 +0000

    first commit for gesis sna day 1

---
+++ GESIS_sna_Day1AM.R
@@ -0,0 +1,17 @@
+#===============================================================================
+# 01-GESIS SNA Day 1
+# Purpose: Day 1 GESIS training in SNA in R
+#   
+# Author: William T Daniel (University of Nottingham)
+#===============================================================================
+
+# PACKAGES
+#===============================================================================
+library(tidyverse)
+# install.packages("alr4") # contains dataset with challenger data
+library(alr4)
+
+
+
+# DATA
+#===============================================================================
