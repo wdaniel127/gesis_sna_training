2a66facc8a3b1fe8b18b2c55ba3a11d45472435d
William T Daniel <william.daniel@nottingham.ac.uk>
2025-03-11 14:15:08 +0000

    end of day 1

--- GESIS_sna_Day1AM.R
+++ GESIS_sna_Day1AM.R
@@ -1,17 +1,25 @@
-#===============================================================================
+# ===============================================================================
 # 01-GESIS SNA Day 1
 # Purpose: Day 1 GESIS training in SNA in R
-#   
+#
 # Author: William T Daniel (University of Nottingham)
-#===============================================================================
+# ===============================================================================
 
 # PACKAGES
-#===============================================================================
+# ===============================================================================
 library(tidyverse)
-# install.packages("alr4") # contains dataset with challenger data
-library(alr4)
+update.packages(ask = FALSE)
 
+##clean up the style of the file
+styler:::style_active_file()
 
+library(RSiena)
+#RSiena, tidyverse, tidygraph, haven, ggraph, cowplot, reshape2, gridExtra, sna,
 
 # DATA
-#===============================================================================
+# ===============================================================================
+#check the working directory
+getwd()
+#check the files in the current directory
+list.files(recursive = TRUE, include.dirs = TRUE)
+
